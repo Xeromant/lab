5060f505781c3393312acd312453cc4d64a4f450
Xeromant <in.e.t@yandex.ru>
2018-04-19 09:22:00 +0300

    main.c changed raznost

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 int
 main ()
 {
-    int a, b, c;
+    int a, b, c, d;
     printf ("Введите два числа\n");
     scanf("%d\n%d", &a, &b);
     
-c=a+b;    printf("%d + %d = %d",a,b,c);
+c=a+b;    d=a-b;    printf("%d + %d = %d; %d - %d = %d",a,b,c,a,b,d);
   return 0;
 }
