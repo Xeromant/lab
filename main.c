0735113612af63cc1086bdd17340127e29d0fd7c
Xeromant <in.e.t@yandex.ru>
2018-04-19 09:37:46 +0300

    add comment

--- main.c
+++ main.c
@@ -7,6 +7,6 @@ main ()
     printf ("Введите два числа\n");
     scanf("%d\n%d", &a, &b);
     
-c=a+b;    d=a-b;    printf("%d + %d = %d; %d - %d = %d",a,b,c,a,b,d);
-  return 0;
+c=a+b;    d=a-b;    printf("%d + %d = %d; %d - %d = %d",a,b,c,a,b,d);   //uchebka
+    return 0;
 }
