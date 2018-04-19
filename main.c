9bc91149395a1f5235827b0835f78370c311b4ce
Xeromant <in.e.t@yandex.ru>
2018-04-19 09:32:13 +0300

    main.c changed clone

--- main.c
+++ main.c
@@ -6,7 +6,8 @@ main ()
     int a, b, c, d;
     printf ("Введите два числа\n");
     scanf("%d\n%d", &a, &b);
-    
-c=a+b;    d=a-b;    printf("%d + %d = %d; %d - %d = %d",a,b,c,a,b,d);
+    c=a+b; 
+    d=a-b;
+    printf("%d + %d = %d; %d - %d = %d",a,b,c,a,b,d);
   return 0;
 }
